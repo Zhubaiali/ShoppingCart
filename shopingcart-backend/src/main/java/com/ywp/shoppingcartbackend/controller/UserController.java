dc9e097653355faac96872a2398f32d697e261d5
liwa <hogwartsghls@gmail.com>
2023-05-28 17:38:59 +0800

    Revert "location接口传参改成json"
    This reverts commit 9535e952af4d86c9d0c439f5c3fb550c5c20f13e.

--- shopingcart-backend/src/main/java/com/ywp/shoppingcartbackend/controller/UserController.java
+++ shopingcart-backend/src/main/java/com/ywp/shoppingcartbackend/controller/UserController.java
@@ -28,9 +28,9 @@ public class UserController {
         User loggedInUser = userService.login(username, password);
         return ResponseEntity.ok(loggedInUser);
     }
-    @PatchMapping("/location/{userId}/")
-    public ResponseEntity<User> updateLocation(@PathVariable Integer userId, @RequestBody Map<String, String> locationInfo) {
-        String location = locationInfo.get("location");
+
+    @PatchMapping("/{userId}/location")
+    public ResponseEntity<User> updateLocation(@PathVariable Integer userId, @RequestParam String location) {
         User updatedUser = userService.updateLocation(userId, location);
         return ResponseEntity.ok(updatedUser);
     }
